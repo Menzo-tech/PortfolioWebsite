5a0ccd5148eb84a395889c358ede128fae3342c7
Mmenim <inyangmmenim@gmail.com>
2024-04-09 05:15:27 -0400

    update created portfolio

--- js/script.js
+++ js/script.js
@@ -14,13 +14,20 @@ const sr = ScrollReveal ({
 
 sr.reveal('.logo',{delay:200, origin: 'left'});
 sr.reveal('.navbar',{delay:300, origin: 'top'})
-sr.reveal('.menu-btn',{delay:500, origin: 'right'});
+sr.reveal('.menu-btn',{delay:350, origin: 'right'});
 
 sr.reveal('.home-text span',{delay:580, origin: 'top'});
 sr.reveal('.home-text h1',{delay:650, origin: 'left'});
 sr.reveal('.home-text p',{delay:700, origin: 'right'});
-sr.reveal('.main-btn',{delay:790, origin: 'left'});
+sr.reveal('.main-btn',{delay:750, origin: 'left'});
 
-sr.reveal('.share',{delay:890, origin: 'buttom'});
+sr.reveal('.share',{delay:850, origin: 'buttom'});
 
 sr.reveal('.home-img',{delay:950, origin: 'right'});
+
+sr.reveal('.about-text span',{delay:580, origin: 'right'});
+sr.reveal('.about-text h3',{delay:580, origin: 'left'});
+sr.reveal('.about-text p',{delay:650, origin: 'buttom'});
+
+sr.reveal('.services-content h5',{delay:650, origin: 'top'});
+sr.reveal('.services-content p',{delay:700, origin: 'buttom'});
